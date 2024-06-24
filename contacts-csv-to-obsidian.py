c0cdf83047017727a7cf44a90854afce0e570e73
Signynt <67801159+Signynt@users.noreply.github.com>
2024-06-24 19:06:58 +0200

    Finished setup

--- contacts-csv-to-obsidian.py
+++ contacts-csv-to-obsidian.py
@@ -5,10 +5,10 @@ import datetime
 import regex as re
 
 # Path to the CSV file
-csv_file = 'contacts-export.csv'
+csv_file = '/Users/vmitchell/GitHub/obsidian-contacts-sync/contacts-export.csv'
 
 # Path to the Contacts folder
-contacts_folder = 'People I Know'
+contacts_folder = '/Users/vmitchell/Documents/Obsidian/Vault/Personal/People I Know'
 
 def represent_none(self, _):
     return self.represent_scalar('tag:yaml.org,2002:null', '')
