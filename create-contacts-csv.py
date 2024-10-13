31fddbc4c09115bc6491ae1320b9bb5407de3d4c
Signynt <67801159+Signynt@users.noreply.github.com>
2024-10-13 14:18:33 +0200

    Updated Obsidian vault path

--- create-contacts-csv.py
+++ create-contacts-csv.py
@@ -72,8 +72,8 @@ def write_to_csv(csv_data, output_file):
             writer.writerow(["First Name", "Last Name", "Address", "Postal Code", "City", "Country"])
             writer.writerows(csv_data)
 
-contacts_path = "/Users/vmitchell/Documents/Obsidian/Vault/Personal/People I Know"
-location_path = "/Users/vmitchell/Documents/Obsidian/Vault/Personal/Locations"
+contacts_path = "/Users/vmitchell/Obsidian/Vault/References/People I Know"
+location_path = "/Users/vmitchell/Obsidian/Vault/References/Locations"
 output_file = "/Users/vmitchell/GitHub/obsidian-contacts-sync/contact-addresses.csv"
 
 csv_data = parse_yaml_files(contacts_path)
